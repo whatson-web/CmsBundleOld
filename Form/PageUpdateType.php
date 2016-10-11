53bf198958526f41abf5aabb20fd966e9e763bce
JF <jf@whatson-web.com>
2016-10-11 10:54:26 +0200

    translations

--- Form/PageUpdateType.php
+++ Form/PageUpdateType.php
@@ -10,71 +10,173 @@ use WH\CmsBundle\Form\Backend\PageBlocType;
 
 /**
  * Class PageUpdateType
+ *
  * @package WH\CmsBundle\Form
  */
 class PageUpdateType extends PageType
 {
 
-    /**
-     * @param FormBuilderInterface $builder
-     * @param array $options
-     */
-    public function buildForm(FormBuilderInterface $builder, array $options)
-    {
+	/**
+	 * @param FormBuilderInterface $builder
+	 * @param array                $options
+	 */
+	public function buildForm(FormBuilderInterface $builder, array $options)
+	{
 
-        parent::buildForm($builder, $options);
+		parent::buildForm($builder, $options);
 
-        $builder
-            ->remove('editer')
-            ->remove('new')
-            ->add('name', 'text')
-            ->add('status', 'choice', array('label' => 'Etat de la page : ', 'choices' => Page::getStatusChoices()))
-            ->add('save_and_stay', 'submit', array('label' => 'Enregistrer', 'attr' => array('class' => 'btn btn-success')))
-            ->add('save_and_back', 'submit', array('label' => 'Enregistrer et quitter ', 'attr' => array('class' => 'btn btn-primary')))
-            ->add('title', 'text', array('label' => 'Titre de la page', 'required' => false))
-            ->add('slugReplace', 'text', array('label' => '', 'required' => false))
-            ->add('slugTechnique', 'text', array('label' => 'Nom technique (ne change pas)', 'required' => false))
-            ->add('url_rewrite', 'text', array('label' => 'Redirection', 'required' => false))
-            ->add('resume', 'textarea', array('label' => 'Résumé', 'required' => false))
-            ->add('body', 'textarea', array('label' => 'Corp de texte', 'required' => false, 'attr' => array('class' => 'tinymce', 'data-theme' => 'advanced')))
-            ->add('thumb', new FileType(), array('label' => false, 'required' => false))
-            ->add('banner', new FileType(), array('label' => false, 'required' => false))
-            ->add('Seo', new SeoType())
-            ->add('template', 'entity', array(
-                'required'      => false,
-                'label'         => 'Template',
-                'class'         => 'APPCmsBundle:Template',
-                'empty_value'   => 'Page normale',
-                'property'      => 'name',
-                'query_builder' => function (TemplateRepository $er) {
+		$builder
+			->remove('editer')
+			->remove('new')
+			->add(
+				'name',
+				'text'
+			)
+			->add(
+				'status',
+				'choice',
+				array(
+					'label'   => 'Etat de la page :',
+					'choices' => Page::getStatusChoices(),
+				)
+			)
+			->add(
+				'save_and_stay',
+				'submit',
+				array(
+					'label' => 'Enregistrer',
+					'attr'  => array('class' => 'btn btn-success'),
+				)
+			)
+			->add(
+				'save_and_back',
+				'submit',
+				array(
+					'label' => 'Enregistrer et quitter',
+					'attr'  => array(
+						'class' => 'btn btn-primary',
+					),
+				)
+			)
+			->add(
+				'title',
+				'text',
+				array(
+					'label'    => 'Titre de la page',
+					'required' => false,
+				)
+			)
+			->add(
+				'slugReplace',
+				'text',
+				array(
+					'label'    => '',
+					'required' => false,
+				)
+			)
+			->add(
+				'slugTechnique',
+				'text',
+				array(
+					'label'    => 'Nom technique',
+					'required' => false,
+				)
+			)
+			->add(
+				'url_rewrite',
+				'text',
+				array(
+					'label'    => 'Redirection',
+					'required' => false,
+				)
+			)
+			->add(
+				'resume',
+				'textarea',
+				array(
+					'label'    => 'Résumé',
+					'required' => false,
+				)
+			)
+			->add(
+				'body',
+				'textarea',
+				array(
+					'label'    => 'Contenu',
+					'required' => false,
+					'attr'     => array(
+						'class'      => 'tinymce',
+						'data-theme' => 'advanced',
+					),
+				)
+			)
+			->add(
+				'thumb',
+				new FileType(),
+				array(
+					'label'    => false,
+					'required' => false,
+				)
+			)
+			->add(
+				'banner',
+				new FileType(),
+				array(
+					'label'    => false,
+					'required' => false,
+				)
+			)
+			->add(
+				'Seo',
+				new SeoType()
+			)
+			->add(
+				'template',
+				'entity',
+				array(
+					'required'      => false,
+					'label'         => 'Template',
+					'class'         => 'APPCmsBundle:Template',
+					'empty_value'   => 'Page normale',
+					'property'      => 'name',
+					'query_builder' => function (TemplateRepository $er) {
 
-                        return $er->createQueryBuilder('Template')->andWhere('Template.type = :type')->setParameter('type', 'page');
+						return $er->createQueryBuilder('Template')->andWhere('Template.type = :type')->setParameter(
+							'type',
+							'page'
+						);
+					},
+				)
+			)
+			->add(
+				'pageBlocs',
+				'collection',
+				array(
+					'type'         => new PageBlocType(),
+					'allow_add'    => true,
+					'allow_delete' => true,
+					'by_reference' => false,
+				)
+			);
+	}
 
-                    }
-            ))
-            ->add('pageBlocs', 'collection', array(
-                'type'         => new PageBlocType(),
-                'allow_add'    => true,
-                'allow_delete' => true,
-                'by_reference' => false
-            ));
-    }
+	/**
+	 * @param OptionsResolverInterface $resolver
+	 */
+	public function setDefaultOptions(OptionsResolverInterface $resolver)
+	{
+		$resolver->setDefaults(
+			array(
+				'data_class' => 'APP\CmsBundle\Entity\Page',
+			)
+		);
+	}
 
-    /**
-     * @param OptionsResolverInterface $resolver
-     */
-    public function setDefaultOptions(OptionsResolverInterface $resolver)
-    {
-        $resolver->setDefaults(array(
-            'data_class' => 'APP\CmsBundle\Entity\Page'
-        ));
-    }
-
-    /**
-     * @return string
-     */
-    public function getName()
-    {
-        return 'wh_cmsbundle_page';
-    }
+	/**
+	 * @return string
+	 */
+	public function getName()
+	{
+		return 'wh_cmsbundle_page';
+	}
 }
