4a0e67ac297b16275b564ad06bcf9da47a0c9334
JF <jf@whatson-web.com>
2016-10-11 10:04:36 +0200

    translation

--- Form/FileType.php
+++ Form/FileType.php
@@ -8,38 +8,73 @@ use Symfony\Component\OptionsResolver\OptionsResolverInterface;
 
 class FileType extends AbstractType
 {
-    /**
-     * @param FormBuilderInterface $builder
-     * @param array $options
-     */
-    public function buildForm(FormBuilderInterface $builder, array $options)
-    {
-        $builder
-            ->add('url', 'elfinder', array('instance'=>'default', 'enable'=>true, 'label' => 'Url : ', 'required' => false))
-            ->add('alt', 'text', array('label' => 'Texte alternatif : ', 'required' => false))
-            ->add('description', 'textarea', array('label' => 'Balise Description : ', 'required' => false))
-            ->add('title', 'text', array('label' => 'Balise title : ', 'required' => false))
-            ->add('position', 'hidden')
-        ;
-    }
 
-    /**
-     * @param OptionsResolverInterface $resolver
-     */
-    public function setDefaultOptions(OptionsResolverInterface $resolver)
-    {
-        $resolver->setDefaults(array(
-            'data_class' => 'APP\CmsBundle\Entity\File'
-        ));
-    }
+	/**
+	 * @param FormBuilderInterface $builder
+	 * @param array                $options
+	 */
+	public function buildForm(FormBuilderInterface $builder, array $options)
+	{
+		$builder
+			->add(
+				'url',
+				'elfinder',
+				array(
+					'instance' => 'default',
+					'enable'   => true,
+					'label'    => 'Url :',
+					'required' => false,
+				)
+			)
+			->add(
+				'alt',
+				'text',
+				array(
+					'label'    => 'Texte alternatif :',
+					'required' => false,
+				)
+			)
+			->add(
+				'description',
+				'textarea',
+				array(
+					'label'    => 'Balise Description :',
+					'required' => false,
+				)
+			)
+			->add(
+				'title',
+				'text',
+				array(
+					'label'    => 'Balise title :',
+					'required' => false,
+				)
+			)
+			->add(
+				'position',
+				'hidden'
+			);
+	}
 
-    /**
-     * @return string
-     */
-    public function getName()
-    {
-        return 'wh_cmsbundle_file';
-    }
+	/**
+	 * @param OptionsResolverInterface $resolver
+	 */
+	public function setDefaultOptions(OptionsResolverInterface $resolver)
+	{
+		$resolver->setDefaults(
+			array(
+				'data_class' => 'APP\CmsBundle\Entity\File',
+			)
+		);
+	}
+
+	/**
+	 * @return string
+	 */
+	public function getName()
+	{
+		return 'wh_cmsbundle_file';
+	}
 }
 
 
